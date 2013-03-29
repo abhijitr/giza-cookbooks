c3dcccd5b4da79ad91947a8df6b6e8c362451ad1
Abhijit Rao <abhijit@pickie.com>
2013-03-29 17:33:38 -0400

    parameterize some stuff

--- giza/recipes/deploy.rb
+++ giza/recipes/deploy.rb
@@ -1,9 +1,21 @@
+include_recipe "nginx::service"
 include_recipe "virtualenv"
 
 node[:deploy].each do |application, deploy|
   if deploy[:application_type] != 'giza'
     Chef::Log.debug("Skipping deploy::web application #{application} as it is not a giza app")
     next
+  end
+
+  opsworks_deploy_dir do
+    user deploy[:user]
+    group deploy[:group]
+    path deploy[:deploy_to]
+  end
+
+  opsworks_deploy do
+    app application
+    deploy_data deploy
   end
 
   directory "#{deploy[:deploy_to]}/shared/#{application}-env" do
@@ -18,19 +30,19 @@ node[:deploy].each do |application, deploy|
     group deploy[:group] 
     owner deploy[:user] 
     action :create
-    requirements_file "#{deploy[:deploy_to]}/current/requirements.txt"
+    requirements_file "#{deploy[:deploy_to]}/current/#{deploy[:requirements_path]}"
   end
 
   # install all the modules in npm_requirements.txt
   execute "npm-dependencies-#{application}" do
-    command "cat #{deploy[:deploy_to]}/current/npm_requirements.txt | xargs npm install -g"
-    only_if "test -f #{deploy[:deploy_to]}/current/npm_requirements.txt"
+    command "cat #{deploy[:deploy_to]}/current/#{deploy[:npm_requirements_path]} | xargs npm install -g"
+    only_if "test -f #{deploy[:deploy_to]}/current/#{deploy[:npm_requirements_path]}"
   end
 
   # start uwsgi under supervisor
   supervisor_service "uwsgi-#{application}" do
     action [:enable, :restart]
-    command "uwsgi --ini-paste-logged production.ini -s /tmp/uwsgi-#{application}.sock -H #{deploy[:deploy_to]}/shared/#{application}-env"
+    command "uwsgi --ini-paste-logged #{deploy[:deploy_to]}/current/#{deploy[:uwsgi_ini_path]} -s /tmp/uwsgi-#{application}.sock -H #{deploy[:deploy_to]}/shared/#{application}-env"
     environment deploy[:environment]
     stopsignal "INT"
     directory "#{deploy[:deploy_to]}/current"
@@ -38,7 +50,7 @@ node[:deploy].each do |application, deploy|
     user deploy[:user] 
   end 
 
-  # reconfigure nginx to override the opsworks defaults
+  # configure nginx 
   nginx_web_app application do
     application deploy
     template "nginx.erb"
