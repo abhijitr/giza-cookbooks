21bd15f553038658d135975c138f7d150e870b40
Abhijit Rao <abhijit@pickie.com>
2013-04-09 16:33:24 -0400

    typo

--- picnic/recipes/setup.rb
+++ picnic/recipes/setup.rb
@@ -6,7 +6,7 @@ supervisor_service "solr-#{node[:solr][:app_name]}" do
   action [:enable, :restart]
   command "/usr/bin/java -jar start.jar"
   stopsignal "INT"
-  directory "#{node[:solr][:home_directory]}/#{node[:solr][:app_name]}"
+  directory "#{node[:solr][:home_dir]}/#{node[:solr][:app_name]}"
   autostart false
   user 'solr' 
 end 
