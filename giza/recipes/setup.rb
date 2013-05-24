6c2a3411f04790c0b67bccfde088dbf5377e4fa1
Abhijit Rao <abhijit@pickie.com>
2013-05-24 18:16:25 -0400

    set up rsyslog properly

--- giza/recipes/setup.rb
+++ giza/recipes/setup.rb
@@ -9,3 +9,39 @@ end
 python_pip "uwsgi" do
   action :upgrade
 end
+
+template "/etc/rsyslog.cfg" do
+  source "rsyslog.conf.erb" 
+  mode 0644
+  variables(
+    :deploy => deploy
+  )
+  notifies :restart, "rsyslog"
+end
+
+template "/etc/rsyslog.d/22-nginx.conf" do
+  source "22-nginx.conf.erb"
+  mode 0644
+  variables(
+    :deploy => deploy
+  )
+  notifies :restart, "rsyslog"
+end
+
+template "/etc/rsyslog.d/23-giza.conf" do
+  source "22-giza.conf.erb"
+  mode 0644
+  variables(
+    :deploy => deploy
+  )
+  notifies :restart, "rsyslog"
+end
+
+template "/etc/rsyslog.d/50-default.conf" do
+  source "50-default.conf.erb"
+  mode 0644
+  variables(
+    :deploy => deploy
+  )
+  notifies :restart, "rsyslog"
+end
