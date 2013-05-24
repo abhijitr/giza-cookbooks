bfd77a73d8bf583ab0f4281e45bcd3901b5a5840
Abhijit Rao <abhijit@pickie.com>
2013-05-24 18:29:01 -0400

    move rsyslog config to deploy phase

--- giza/recipes/deploy.rb
+++ giza/recipes/deploy.rb
@@ -77,4 +77,41 @@ node[:deploy].each do |application, deploy|
       :deploy => deploy
     )
   end
+
+  # update rsyslog config
+  template "/etc/rsyslog.cfg" do
+    source "rsyslog.conf.erb" 
+    mode 0644
+    variables(
+      :deploy => deploy
+    )
+    notifies :restart, "rsyslog"
+  end
+  
+  template "/etc/rsyslog.d/22-nginx.conf" do
+    source "22-nginx.conf.erb"
+    mode 0644
+    variables(
+      :deploy => deploy
+    )
+    notifies :restart, "rsyslog"
+  end
+  
+  template "/etc/rsyslog.d/23-giza.conf" do
+    source "22-giza.conf.erb"
+    mode 0644
+    variables(
+      :deploy => deploy
+    )
+    notifies :restart, "rsyslog"
+  end
+  
+  template "/etc/rsyslog.d/50-default.conf" do
+    source "50-default.conf.erb"
+    mode 0644
+    variables(
+      :deploy => deploy
+    )
+    notifies :restart, "rsyslog"
+  end
 end
