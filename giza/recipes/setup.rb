1ca44e02e06b7a10d3c307f2785023000268bc79
Abhijit Rao <abhijit@pickie.com>
2013-05-25 14:20:07 -0400

    logrotation

--- giza/recipes/setup.rb
+++ giza/recipes/setup.rb
@@ -6,6 +6,29 @@ package "libpq-dev" do
   action :install
 end
 
+package "lzop" do
+  action :install
+end
+
+python_pip "awscli" do
+  action :upgrade
+end
+
 python_pip "uwsgi" do
   action :upgrade
+end
+
+cookbook_file "/etc/logrotate.conf" do
+  path "logrotate.conf"
+  action :create
+end
+
+cookbook_file "/etc/logrotate.d/nginx" do
+  path "logrotate_nginx"
+  action :create
+end
+
+cookbook_file "/etc/logrotate.d/giza" do
+  path "logrotate_giza"
+  action :create
 end
